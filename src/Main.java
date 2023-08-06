7a6501feed525a16f9680108f178fda173aab57f
raonieqr <raoniestevan@gmail.com>
2023-08-05 21:44:58 -0300

    add: option for taskAlarm in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import entities.FileUtils;
 import entities.PrintLog;
 import entities.Task;
+import entities.TaskAlarm;
 import enums.Priority;
 import enums.Status;
 
@@ -25,23 +26,25 @@ public class Main
 		}
 		return false;
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Bem vindo a TODO-List");
 		ArrayList<Task> tasks = new ArrayList<>();
+		ArrayList<Task> taskWithAlarm = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		int option = 0;
 		int idCounter = 1;
-		while (option != 3) {
+		while (option != 4) {
 			System.out.println("Escolha uma opções abaixo");
 			System.out.println("1 - Criar tarefa");
 			System.out.println("2 - Visualizar tarefas");
-			System.out.println("3 - Sair");
+			System.out.println("3 - Aguardar alarme");
+			System.out.println("4 - Sair");
 			while (true) {
 				try {
 					option = Integer.parseInt(sc.nextLine());
-					if (option >= 1 && option <= 3)
+					if (option >= 1 && option <= 4)
 						break;
-					System.out.println("Error: Opção inválida. Escolha uma opção entre 1 e 3.");
+					System.out.println("Error: Opção inválida. Escolha uma opção entre 1 e 4.");
 				} catch (Exception e) {
 					System.out.println("Error: Entrada inválida. Digite um número válido.");
 				}
@@ -73,8 +76,26 @@ public class Main
 					System.out.println("Error: Formato inválido. Formato correto dd/MM/yyyy HH:mm:ss");
 					continue;
 				}
+				System.out.println("Deseja colocar alarme para esta tarefa?");
+				System.out.println("1 - Sim");
+				System.out.println("2 - Não");
+				int alarm;
+				while (true) {
+					try {
+						alarm = Integer.parseInt(sc.nextLine());
+						if (alarm == 1 || alarm == 2)
+							break;
+					}
+					catch (NumberFormatException e) {
+						System.out.println("Error: Entrada inválida. Tente novamente");
+					}
+				}
 				Task task = new Task(idCounter++, name, description, category, priority, status, dateTime);
 				tasks.add(task);
+				if (alarm == 1) {
+					taskWithAlarm.add(task);
+					System.out.println("Criado alarme para a tarefa");
+				}
 				if (tasks.size() > 1)
 					Collections.sort(tasks, Comparator.comparing(Task::getDateTime).thenComparing(Task::getPriority));
 				System.out.println("Tarefa criada");
@@ -85,13 +106,19 @@ public class Main
 				else
 					PrintLog.showTask(tasks);
 			}
+			if (option == 3) {
+				TaskAlarm taskAlarm = new TaskAlarm();
+				taskAlarm.run(taskWithAlarm);
+			}
 		}
-		File currentDirectory = new File(".");
-		if (currentDirectory.getParent() == null)
-			FileUtils.writeToFile(tasks, new File(currentDirectory, "tasks.txt").getPath());
-		else {
-			File parentDirectory = currentDirectory.getParentFile();
-			FileUtils.writeToFile(tasks, new File(parentDirectory, "tasks.txt").getPath());
+		if (!tasks.isEmpty()) {
+			File currentDirectory = new File(".");
+			if (currentDirectory.getParent() == null)
+				FileUtils.writeToFile(tasks, new File(currentDirectory, "tasks.txt").getPath());
+			else {
+				File parentDirectory = currentDirectory.getParentFile();
+				FileUtils.writeToFile(tasks, new File(parentDirectory, "tasks.txt").getPath());
+			}
 		}
 	}
 }
