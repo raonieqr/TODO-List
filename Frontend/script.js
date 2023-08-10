af0fefe8cca6e3e14f56d1619d08c389956cd59d
raonieqr <raoniestevan@gmail.com>
2023-08-09 21:32:04 -0300

    add: remove task, list task

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var btn = document.getElementById("createTask");
-    var formContainer = document.getElementById("taskForm");
-
+    let btn = document.getElementById("createTask");
+    let sectionAdd = document.getElementById("taskForm");
+    let homeSection = document.getElementById("home")
     btn.addEventListener("click", function() {
-        btn.style.display = "none";
-        formContainer.style.display = "block";
-        formContainer.scrollIntoView({ behavior: "smooth" });
+        btn.classList.add = "hiddenItem";
+        homeSection.style.display = "none";
+        sectionAdd.style.display = "flex";
+        sectionAdd.scrollIntoView({ behavior: "smooth" });
         
     });
 
-    var btnSubmit = document.getElementById("addTask");
+    let btnSubmit = document.getElementById("addTask");
     let task = {};
     let taskArray = [];
+    let id = 1;
     btnSubmit.addEventListener("click", function() {
         event.preventDefault();
-        
-        var name = document.getElementById("name")
-        var description = document.getElementById("description")
-        var category = document.getElementById("category")
-        var priority = document.getElementById("priority")
-        var status = document.getElementById("status")
-        var dateTime = document.getElementById("dateTime")
+        let name = document.getElementById("name")
+        let description = document.getElementById("description")
+        let category = document.getElementById("category")
+        let priority = document.getElementById("priority")
+        let status = document.getElementById("status")
+        let dateTime = document.getElementById("dateTime")
         
         task = {
+            id: id,
             name: name.value,
             description: description.value,
             category: category.value,
@@ -30,8 +32,10 @@ document.addEventListener("DOMContentLoaded", function() {
             status: status.value,
             dateTime: dateTime.value
         }
-
+        
         taskArray.push(task);
+        alert("Tarefa " + task.name + " foi criada");
+        id++;
         name.value = "";
         description.value = "";
         category.value = "";
@@ -40,5 +44,66 @@ document.addEventListener("DOMContentLoaded", function() {
         dateTime.value = "";
         console.log(taskArray);
 
+    })
+
+    let btnShow = document.getElementById("showTask");
+    let table = document.getElementById("tasks");
+    let btnAddTaskList = document.getElementById("addTaskList")
+    btnShow.addEventListener("click", function() {
+        btnShow.style.display = "none";
+        sectionAdd.style.display = "none";
+        btnAddTaskList.style.display = "inline-block";
+        table.style.display = "block";
+
+        table.scrollIntoView({ behavior: "smooth" });
+    
+        let tbody = document.createElement("tbody");
+        for (let i = 0; i < taskArray.length; i++) {
+            let task = taskArray[i];
+            
+            let row = document.createElement("tr");
+    
+            let c1 = document.createElement("td");
+            let c2 = document.createElement("td");
+            let c3 = document.createElement("td");
+            let c4 = document.createElement("td");
+            let c5 = document.createElement("td");
+            let c6 = document.createElement("td");
+            let c7 = document.createElement("td");
+            let c8 = document.createElement("td");
+            let c9 = document.createElement("td");
+
+    
+            c1.innerText = task.id;
+            c2.innerText = task.name;
+            c3.innerText = task.description;
+            c4.innerText = task.category; 
+            c5.innerText = task.priority; 
+            c6.innerText = task.status; 
+            c7.innerText = task.dateTime;
+            c8.innerHTML = `<i id="pencil" class=\"ph ph-pencil-line\"></i>
+            <i id="trash" class=\"ph ph-trash\"></i>`;
+            row.appendChild(c1);
+            row.appendChild(c2);
+            row.appendChild(c3);
+            row.appendChild(c4);
+            row.appendChild(c5);
+            row.appendChild(c6);
+            row.appendChild(c7);
+            row.appendChild(c8);
+            tbody.appendChild(row);
+            table.appendChild(tbody);
+
+            ((index) => {
+                let trashIcon = row.querySelector("#trash");
+                trashIcon.addEventListener("click", function() {
+                    taskArray.splice(index, 1);
+                    tbody.removeChild(row);
+                    console.log(taskArray.length)
+                });
+            })(i);
+
+
+        }
     })
 })
