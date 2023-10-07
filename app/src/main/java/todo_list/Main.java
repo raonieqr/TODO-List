882f1e2842b112c9707928ad37f96365142bd802
raonieqr <raoniestevan@gmail.com>
2023-10-07 00:06:07 -0300

    refactor: main

--- app/src/main/java/todo_list/Main.java
+++ app/src/main/java/todo_list/Main.java
@@ -24,80 +24,80 @@ public class Main
 
 		ArrayList<Task> tasks = new ArrayList<>();
 		ArrayList<Task> taskWithAlarm = new ArrayList<>();
+		FileManager fileManager = new FileManager();
 		Scanner sc = new Scanner(System.in);
 
 		int option = 0;
-		int idCounter = 1;
-
 		while (option != 6) {
 
 			TaskView.showMenu();
 
 			option = InputValidator.promptForIntegerInput("Digite o número: ");
-			if (option == 1) {
-				Task task = TaskView.createTaskFromUserInput(sc);
-				TaskView.createTaskAlarm(task, tasks, taskWithAlarm);
-				System.out.println("Tarefa criada");
-			}
-			if (option == 2) {
-				if (tasks.isEmpty())
-					System.out.println("A lista de tarefas está vazia");
-				else
-					TaskView.showTask(tasks);
-			}
-			if (option == 3) {
-				TaskAlarm taskAlarm = new TaskAlarm();
-				taskAlarm.run(taskWithAlarm);
-			}
-			if (option == 4) {
-				if (tasks.isEmpty()) {
-					System.out.println("Erro: Lista vazia. Tente novamente");
-					continue;
-				}
-				for(Task task: tasks)
-					System.out.println(task);
-				System.out.println("Escolha o id da tarefa que gostaria de editar:");
-				int index = Integer.parseInt(sc.nextLine());
-				for (Task task: tasks) {
-					if (task.getId() == index) {
-						System.out.println("Qual o novo status da tarefa? ");
-						System.out.println("1 - Feito");
-						System.out.println("2 - Em andamento");
-						System.out.println("3 - A fazer");
-						String status = sc.nextLine();
-						while(!status.matches("^[1-3]$")) {
-							System.out.println("Erro: Status inválido. Escolha um número entre 1 e 3.");
-							status = sc.nextLine();
-						}
-						task.setStatus(Status.fromValue(Integer.parseInt(status)));
+			switch (option) {
+				case 1:
+					Task task = TaskView.createTaskFromUserInput(sc);
+
+					TaskView.createTaskAlarm(task, tasks, taskWithAlarm);
+
+					System.out.println("Tarefa criada");
+
+					break;
+				case 2:
+					if (tasks.isEmpty())
+						System.out.println("A lista de tarefas está vazia");
+					else
+						TaskView.showTask(tasks);
+
+					break;
+				case 3:
+					TaskAlarm taskAlarm = new TaskAlarm();
+					taskAlarm.run(taskWithAlarm);
+
+					break;
+				case 4:
+					if (tasks.isEmpty()) {
+						System.out.println("Erro: Lista vazia. Tente novamente");
+						return;
 					}
-				}
-			}
-			if(option == 5) {
-				if (tasks.isEmpty()) {
-					System.out.println("Erro: Lista vazia. Tente novamente");
-					continue;
-				}
-				for(Task task: tasks)
-					System.out.println(task);
-				System.out.println("Digite o id da tarefa que gostaria de deletar:");
-				int index = Integer.parseInt(sc.nextLine());
-				if (index >= 0 && (index - 1) <= tasks.size())
-					tasks.remove((index - 1));
-				else
-					System.out.println("Erro: Index não encontrado");
+					tasks.forEach(System.out::println);
+					System.out.println("Escolha o id da tarefa que gostaria de editar:");
+					int index = Integer.parseInt(sc.nextLine());
+					for (Task selectedTask : tasks) {
+						if (selectedTask.getId() == index) {
+							System.out.println("Qual o novo status da tarefa? ");
+							System.out.println("1 - Feito");
+							System.out.println("2 - Em andamento");
+							System.out.println("3 - A fazer");
+							String status = sc.nextLine();
+							while (! status.matches("^[1-3]$")) {
+								System.out.println("Erro: Status inválido. Escolha um número entre 1 e 3.");
+								status = sc.nextLine();
+							}
+							selectedTask.setStatus(Status.fromValue(Integer.parseInt(status)));
+						}
+					}
+					break;
+				case 5:
+					if (tasks.isEmpty()) {
+						System.out.println("Erro: Lista vazia. Tente novamente");
+						return;
+					}
+
+					tasks.forEach(System.out::println);
+
+					System.out.println("Digite o ID da tarefa que gostaria de deletar:");
+					int idDelete = Integer.parseInt(sc.nextLine());
+
+					if (idDelete >= 1 && idDelete <= tasks.size()) {
+						tasks.remove(idDelete - 1);
+						System.out.println("Tarefa removida com sucesso.");
+					}
+					else
+						System.out.println("Erro: Índice não encontrado");
+					break;
+
 			}
 		}
-		if (!tasks.isEmpty()) {
-			File currentDirectory = new File(".");
-			if (currentDirectory.getParent() == null)
-				FileManager.writeToFile(tasks, new File(currentDirectory, "tasks.txt").getPath());
-			else {
-				File parentDirectory = currentDirectory.getParentFile();
-				FileManager.writeToFile(tasks, new File(parentDirectory, "tasks.txt").getPath());
-			}
-		}
+		fileManager.createFile(tasks);
 	}
-
-
 }
--- app/src/main/java/todo_list/utils/FileManager.java
+++ app/src/main/java/todo_list/utils/FileManager.java
@@ -1,27 +1,39 @@
 package todo_list.utils;
 
+import todo_list.entities.Task;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
-	public static void writeToFile(ArrayList<?> tasks, String filePath) {
+	public void writeToFile(List<?> tasks, String filePath) {
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
 			File file = new File(filePath);
-			if (!file.exists()) {
+			if (!file.exists())
 				file.createNewFile();
-			}
 
 			for (Object item : tasks) {
 				writer.write(item.toString());
 				writer.newLine();
 			}
 			writer.close();
-			System.out.println("Conteúdo salvo em " + filePath);
 		} catch (IOException e) {
-			System.out.println("Error: Não foi possível gravar o arquivo " + filePath);
+			System.out.println(filePath);
+		}
+	}
+
+	public void createFile(List<Task> tasks) {
+		if (!tasks.isEmpty()) {
+			File currentDirectory = new File(".");
+			if (currentDirectory.getParent() == null)
+				this.writeToFile(tasks, new File(currentDirectory, "tasks.txt").getPath());
+			else {
+				File parentDirectory = currentDirectory.getParentFile();
+				this.writeToFile(tasks, new File(parentDirectory, "tasks.txt").getPath());
+			}
 		}
 	}
 }
